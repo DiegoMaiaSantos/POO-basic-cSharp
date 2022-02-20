949869198f12fd1b8f3c079e51dc43babeccb11d
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-20 11:58:16 -0300

    Criação de método POO.
    Foi criado um método para facilitar no calculo de um triângulo.
    
    Exemplo:
    
    public double Area()
            {
                double p = (A + B + C) / 2.0;
                return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
            }

--- resolvendo-prob-sem-OO/Triangulo.cs
+++ resolvendo-prob-sem-OO/Triangulo.cs
@@ -1,9 +1,17 @@
-namespace Course 
+using System;
+
+namespace Course
 {
-    class Triangulo 
+    class Triangulo
     {
-        public double A;   
-        public double B;   
-        public double C;  
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));                        
+        }
     }
 }
