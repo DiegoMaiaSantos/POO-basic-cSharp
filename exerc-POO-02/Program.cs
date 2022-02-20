686bf4aab6e23df2343160b4f1795cbdb4b10f3f
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-20 16:22:49 -0300

    Exercício 02 para fixar conteúdo.

---
+++ exerc-POO-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Funcionario fun = new Funcionario();
+
+            Console.WriteLine("========= PLANILHA DE DADOS =========");            
+            Console.WriteLine("====================================="); 
+            Console.Write("Nome: ");
+            fun.Nome = Console.ReadLine();
+            Console.Write("Salário bruto R$: ");
+            fun.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto R$: ");
+            fun.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("====================================="); 
+            Console.WriteLine("Funcionário: " + fun);
+
+            Console.WriteLine("====================================="); 
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porce = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            fun.AumentarSalario(porce);
+
+            Console.WriteLine("====================================="); 
+            Console.WriteLine("Dados atualizados: " + fun);
+
+            Console.WriteLine("====================================="); 
+        }
+    }
+}
