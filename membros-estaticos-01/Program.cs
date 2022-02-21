5a01c91b8c0d7c6013e8ca933428f2c00381bac1
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-21 18:26:36 -0300

    Membros estáticos Parte 01.

---
+++ membros-estaticos-01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========================");
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("========================");
+        }
+
+        static double Circunferencia(double ra)
+        {
+            return 2.0 * Pi * ra;
+        }
+
+        static double Volume(double ra)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(ra, 3.0);
+        }
+    }
+}
