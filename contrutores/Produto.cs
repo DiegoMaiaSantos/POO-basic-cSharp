e4288b36574e8042a89a19b565ca62811cfcc506
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-22 08:58:10 -0300

    Construtores -Novo conteúdo.

---
+++ contrutores/Produto.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$: " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidade(s), total R$: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ contrutores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== CADASTRO E CONTRO DE PRODUTOS ==");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Digite os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço R$: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("===================================");
+            Console.Write("Digite o número de produtos a serem adicionados: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine("===================================");
+            Console.Write("Digite o número de produtos a serem removidos: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
