c26ecfbf60ba07af299afd7ed9db144c9a6041ea
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-03 08:11:35 -0300

    Exercício de fixação de encapsulamento.

---
+++ exerc-fixacao-03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine("================ CONTA BANCARIA ================");
+            Console.WriteLine("================================================");
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n) ");
+            char escolha = char.Parse(Console.ReadLine());
+            if (escolha == 's' || escolha == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial R$: ")
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                {
+
+                }
+            }
+        }
+    }
+}
