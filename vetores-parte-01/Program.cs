400925ac91cf5dee98bccd70443aeea1b93b9d92
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-05 15:36:38 -0300

    Vetores parte 01 finalizado.

--- vetores-parte-01/Program.cs
+++ vetores-parte-01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());     
+
+            double[] vect = new double[n]; 
+
+            double soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                soma += vect[i];
+            } 
+
+            double media = soma / n;   
+
+            Console.WriteLine("A média das alturas é: " + ((float)media).ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
