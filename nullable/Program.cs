b63e2e4df202340a33125c12f4cd8ae53e79e61a
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-05 10:22:10 -0300

    Conteúdo sobre nullable.
    Exemplo:
    
    double? x = null;
    double? y = 10;
    
    double a = x ?? 5;
    double b = y ?? 5;

---
+++ nullable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); // Vai pegar o valor atribuido ou 
+            Console.WriteLine(y.GetValueOrDefault()); // o valor padrão.
+
+            Console.WriteLine(x.HasValue); // Para saber se tem algum valor.
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null.");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null.");
+            }
+        }
+    }
+}
