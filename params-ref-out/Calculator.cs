0bcc5dc238cbfa3c56da77f9705a04bd5f9920d2
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-06 14:40:36 -0300

    Conteúdo: params ref e out.
    Evitar de usar pois são code smells.

---
+++ params-ref-out/Calculator.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Calculator
+    {
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
