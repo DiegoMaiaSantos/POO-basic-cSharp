8b131fbb510106cc03475ad95b6098b76aad080b
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-08 22:56:08 -0300

    Conteúdo: foreach.

---
+++ foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
