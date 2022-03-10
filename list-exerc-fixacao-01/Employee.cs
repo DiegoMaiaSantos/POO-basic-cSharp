f943ac04b780669ed3b21758953ad87ace17a803
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-03-10 17:53:21 -0300

    Exercício fixação list - 01.

---
+++ list-exerc-fixacao-01/Employee.cs
@@ -0,0 +1,9 @@
+using System.Globalization;
+
+namespace Course
+{
+    class Employee
+    {
+        
+    }
+}
---
+++ list-exerc-fixacao-01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static Void Main(string[] args)
+        {
+            
+        }
+    }
+}
